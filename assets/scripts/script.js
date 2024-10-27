1dc3d1737f3b978c51ed01f9323d67179a8f618a
Eric Faraut <eric.faraut@hotmail.fr>
2024-10-27 15:51:21 +0100

    Étape 2 is done.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,18 @@
 let flecheGauche = document.querySelector(".arrow_left");
 let flecheDroite = document.querySelector(".arrow_right");
 
-flecheGauche.addEventListener("click", () => {
-	alert("Vous voulez afficher l'image précédente du carrousel.");
-});
-flecheDroite.addEventListener("click", () => {
-	alert("Vous voulez afficher l'image suivante du carrousel.");
-});
+/**
+ * Cette fonction est DRY et 
+ * facilite l'éventuel usage futur de removeEventListener.
+ * @param {*} avantOuApres selon que la flèche est gauche ou droite le msg réfèrera
+ * respectivement à l'image précédente ou suivante du carrousel.
+ */
+function affichage(avantOuApres) {
+	alert(`Vous voulez afficher l'image ${avantOuApres} du carrousel.`);
+}
+
+const affichageGauche = affichage.bind(null, "précédente");
+const affichageDroite = affichage.bind(null, "suivante");
+
+flecheGauche.addEventListener("click", affichageGauche);
+flecheDroite.addEventListener("click", affichageDroite);
